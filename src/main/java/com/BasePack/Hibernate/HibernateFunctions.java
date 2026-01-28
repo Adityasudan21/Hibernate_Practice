e00699aa960584070eeec26e17a97f4c48cf80bd
Adityasudan21 <adityasudan21@gmail.com>
2026-01-28 21:56:56 +0530

    Made the Code SOLID based and created functions and increased readability and added read function.

---
+++ src/main/java/com/BasePack/Hibernate/HibernateFunctions.java
@@ -0,0 +1,50 @@
+package com.BasePack.Hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateFunctions {
+    public Student Read(Student s,String primary_key){
+        SessionFactory sf=new Configuration()
+                .addAnnotatedClass(com.BasePack.Hibernate.Student.class)
+                .configure()
+                .buildSessionFactory();
+        Session s1=sf.openSession();
+        //Main line
+        s=s1.find(Student.class,primary_key);
+
+        s1.close();
+        sf.close();
+        return s;
+    }
+    public boolean insert(Student s){
+
+//        Configuration cfg=new Configuration();
+//        cfg.addAnnotatedClass(com.BasePack.Hibernate.Student.class); //This is to Let hibernate know where to find
+//        // the annotated file
+//        cfg.configure();  //Configure through the XML provided
+//        SessionFactory sf=cfg.buildSessionFactory(); //Heavyweight close and open mindfully
+
+        //WRITE ABOVE OR USE CON-CAT to write in a single line like below
+        SessionFactory sf=new Configuration()
+                .addAnnotatedClass(com.BasePack.Hibernate.Student.class)
+                .configure()
+                .buildSessionFactory(); //Heavyweight close and open mindfully
+
+
+
+        Session session=sf.openSession(); //lightweight open close when needed
+
+        //Main Line
+        session.persist(s);   // .save has been removed in Hibernate 7.1.0
+
+        Transaction trans= session.beginTransaction();
+        //After this we need to save this (OR COMMIT this) This is done like a transaction
+        trans.commit();
+        session.close();
+        sf.close();
+        return s != null;
+    }
+}
--- src/main/java/com/BasePack/Hibernate/Main.java
+++ src/main/java/com/BasePack/Hibernate/Main.java
@@ -6,35 +6,27 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
+    static HibernateFunctions Functions=new HibernateFunctions();
     public static void main(String[] args){
         Student s1=new Student();
-        s1.setRollNo(3);
-        s1.setsName("Harsh");
-        s1.setsAge(28);
+        s1.setRollNo(5);
+        s1.setsName("Vishka");
+        s1.setsAge(19);
 
-//        Configuration cfg=new Configuration();
-//        cfg.addAnnotatedClass(com.BasePack.Hibernate.Student.class); //This is to Let hibernate know where to find
-//        // the annotated file
-//        cfg.configure();  //Configure through the XML provided
-//        SessionFactory sf=cfg.buildSessionFactory(); //Heavyweight close and open mindfully
+//        //Insert in Table
+//        if(Functions.insert(s1))
+//            System.out.println("Object Inserted in Table Student");
+//        else
+//            System.out.println("Can't Insert Object in the Table Student");
 
-        //WRITE ABOVE OR USE CON-CAT to write in a single line like below
-        SessionFactory sf=new Configuration()
-                .addAnnotatedClass(com.BasePack.Hibernate.Student.class)
-                .configure()
-                .buildSessionFactory(); //Heavyweight close and open mindfully
+        Student s2=null;
 
+        //Read the value of a record with the primary key reference
+        s2=Functions.Read(s2,"5");
+        if(s2==null)
+            System.out.println("No Record Found in Table Student");
+        else
+            System.out.println(s2.toString());
 
-
-        Session session=sf.openSession(); //lightweight open close when needed
-        session.persist(s1);   // .save has been removed in Hibernate 7.1.0
-
-        Transaction trans= session.beginTransaction();
-        //After this we need to save this (OR COMMIT this) This is done like a transaction
-        trans.commit();
-
-        session.close();
-        sf.close();
-        System.out.println(s1.toString());
     }
 }
